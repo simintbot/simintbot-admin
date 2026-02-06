74a2df38cc977d4b106e7b7c483938bd5c3b4143
Sylvère O. AKAMBI <akambi.sylv@gmail.com>
2026-02-06 22:14:40 +0100

    feat: deploy

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,12 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   output: "standalone",
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1';
+    console.log('Proxy configuring to:', apiUrl);
     return [
       {
         source: '/api/proxy/:path*',
-        destination: process.env.NODE_ENV === 'production' 
-          ? 'https://api.simint-bot.com/api/v1/:path*'
-          : 'https://devapi.simint-bot.com/api/v1/:path*',
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
